6c87a04986353b15b94cdca1b31615b8549b0c3e
Ali Shahidi <alishahidi1376@gmail.com>
2025-03-28 19:56:39 +0330

    using centeral + action

--- sbcore-starter-test/src/test/java/io/github/alishahidi/sbcore/SBCoreApplicationTests.java
+++ sbcore-starter-test/src/test/java/io/github/alishahidi/sbcore/SBCoreApplicationTests.java
@@ -3,11 +3,11 @@ package io.github.alishahidi.sbcore;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class ApiApplicationTests {
+@SpringBootTest(classes = SBCoreApplication.class)
+class SBCoreApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
