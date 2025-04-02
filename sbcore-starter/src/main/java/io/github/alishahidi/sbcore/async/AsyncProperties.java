346f7ea27e2c465769a03f6c7fcdf00a2d320bb4
hamed <theist.thirteenmm@gmail.com>
2025-04-02 04:32:49 +0330

    update new package -Async and -SpringContext

--- sbcore-starter/src/main/java/io/github/alishahidi/sbcore/async/AsyncProperties.java
+++ sbcore-starter/src/main/java/io/github/alishahidi/sbcore/async/AsyncProperties.java
@@ -1,8 +1,13 @@
 package io.github.alishahidi.sbcore.async;
 
+import jakarta.persistence.Entity;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
-
+@Getter
+@Setter
 @Component
 @ConfigurationProperties(prefix = "sbcore.async")
 public class AsyncProperties {
@@ -10,29 +15,4 @@ public class AsyncProperties {
     private int corePoolSize = 5;    // مقدار پیش‌فرض
     private int maxPoolSize = 10;    // مقدار پیش‌فرض
     private int queueCapacity = 500; // مقدار پیش‌فرض
-
-    // Getter و Setter برای هر فیلد
-    public int getCorePoolSize() {
-        return corePoolSize;
-    }
-
-    public void setCorePoolSize(int corePoolSize) {
-        this.corePoolSize = corePoolSize;
-    }
-
-    public int getMaxPoolSize() {
-        return maxPoolSize;
-    }
-
-    public void setMaxPoolSize(int maxPoolSize) {
-        this.maxPoolSize = maxPoolSize;
-    }
-
-    public int getQueueCapacity() {
-        return queueCapacity;
-    }
-
-    public void setQueueCapacity(int queueCapacity) {
-        this.queueCapacity = queueCapacity;
-    }
 }
